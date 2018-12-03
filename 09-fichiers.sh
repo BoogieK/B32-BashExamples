fd3ab66201e29c5c49a2352ea077ddccb7a8c869
Kim Charbonneau <kim.charbonneau@outlook.com>
2018-12-03 11:16:31 -0500

    Explication des fichiers

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+#test -e : Est-ce que ce fichier la existe
+if test -e $1
+then 
+	echo "existe"
+	
+	# -d : Est-ce que c'est un directory
+	if test -d $1
+	then 
+		echo "Il est un répertoire"
+	fi
+else
+	echo "inexistant"
+fi
