8d253476acf152aebc988ecb216c5e6c9dac1d8e
Kim Charbonneau <kim.charbonneau@outlook.com>
2018-12-03 10:57:34 -0500

    Explication tableau

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+liste=`ls`
+
+#Pour chaque éléments de la liste fais ...
+for fic in $liste
+do
+	echo $fic
+done
+
+resultat=o
+
+# -o = or
+# -a = and
+
+while test $resultat = 0 -o $resultat = o
+do
+	echo -p "Entrer 0 pour répéter la boucle" resultat
+done
--- 06-tableau.sh
+++ 06-tableau.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+declare -a nom=(Back to the Future) 
+
+#Afficher le premier élément du tableau
+echo ${nom[0]}
+echo ${nom[1]}
+# Combien d'éléments dans mon tableau
+echo ${#nom[@]}
+
