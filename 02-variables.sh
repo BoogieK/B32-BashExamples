7736c493f89ef4380dfd391bf1030f325173824a
Kim Charbonneau <kim.charbonneau@outlook.com>
2018-12-03 10:02:36 -0500

    Explication variables syteme

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+# Ne pas mettre d'espace
+prenom=KimCharles
+echo $prenom
+
+# Forcer une déclaration d'une variable "int"
+declare -i age=33
+
+# Mettre une variable READ ONLY
+declare -r age2=33
+
+# Les variables système 
+# Nom de l'exécutable
+echo $0
+#Premier argument quand t'as appeler ton executable
+echo $1
+echo $2
+#Combien d'arguments on ete passés
+echo $#
+
