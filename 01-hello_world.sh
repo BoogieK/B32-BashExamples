52ad81443d5a5874cf04e1e23b111d31108540e4
Kim Charbonneau <kim.charbonneau@outlook.com>
2018-12-03 09:49:56 -0500

    Commande de base variable et affichage

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+# Ceci est un commentaire
+echo "Salut"
+
+# -n = empeche retour de ligne
+echo -n "Vive "
+echo "Linux"
+
+# -e = introduit les caracteres speciaux
+echo -e "123\n123\n123"
+
+# read= comme un cin
+# p = specifie un message
+# Quand on assigne une variable, on n a pas besoin d'un signe de $
+# $variable (quand on veut utiliser la variable dans la variable)
+read -p "Vous avez quel age? " age
+echo $age "ans! Vous paraissez plus jeune"
