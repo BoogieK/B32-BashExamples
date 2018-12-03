c0397fcc26016c366c417f3d25843d64c6781f97
Kim Charbonneau <kim.charbonneau@outlook.com>
2018-12-03 11:49:13 -0500

    Exercise ou l'on demande un mot à l'utilisateur et on recherche celui-ci dans une base de données

--- exercice1.sh
+++ exercice1.sh
@@ -2,4 +2,15 @@
 
 # La ligne suivante permet de vérifier si un mot existe dans le dictionnaire
 # Il faut suffixer cet URL avec le mot entré par l'uasger (ex: word=chat)
-#wget -qO - http://dictionary.objectif8.com/exists.php?word=
+
+read -p "Entrer le mot a rechercher : " word
+
+resultat=`wget -qO - http://dictionary.objectif8.com/exists.php?word=$word`
+
+if test $resultat = "1"
+then 
+	echo "Le mot existe"
+else
+	echo "Le mot n'existe pas..."
+fi
+
