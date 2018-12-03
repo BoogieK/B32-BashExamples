db89d2d6cadd6ce5976bd0de9af23f072d44d2cb
Kim Charbonneau <kim.charbonneau@outlook.com>
2018-12-03 10:09:11 -0500

    Explication calculs

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+# Afficher la formule
+echo 10+19
+# Quand tu veux afficher la valeur du calcul
+echo $((10+19))
+
+read -p "Nombre 1 : " premier
+read -p "Nombre 2 : " second
+
+let resultat=$premier*$second
+
+echo $resultat
