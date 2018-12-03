7d053e186f335e8d6e75478a12c0caef00eb336f
Kim Charbonneau <kim.charbonneau@outlook.com>
2018-12-03 12:06:56 -0500

    Exercise ou l'on demande a l'utilisateur de choisir une option. On appel un exécutable autre ou on quitte

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+Menu(){
+	clear
+	echo ""
+	echo " ======== CVM SOFT ======="
+	echo "|     d = Dictionnaire   |"
+	echo "|     q = Quitter        |"
+	echo " ========================="
+}
+
+Menu
+read -p "Quel est votre choix : " choix
+
+if test $choix = "d"
+then 
+	./exercice1.sh
+else
+	echo "Quitter"
+fi
