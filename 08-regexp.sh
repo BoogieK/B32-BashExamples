11c6aac740d6cdc415dd3f7dc14b733062375b39
Kim Charbonneau <kim.charbonneau@outlook.com>
2018-12-03 11:11:22 -0500

    Explication expressions regulieres

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+echo -n "Texte : "
+read texte
+
+#Commence par 0 a 9, plein d'autre après puispas autre chose à la fin. Pas de lettre.
+# ~ Nous averti que c'estv une expression régulière. On en lit pas ^[0-9].. mais bine de ne pas avoir de lettre et de ne pas depasser 99999999999999999999999999999999999...
+if [[ $texte =~ ^[0-9]+$ ]]
+then
+	echo "Numérique"
+fi
